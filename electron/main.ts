fb3fb93d3a8c3e13607693d29bb5801211321042
joaomjbraga <joomdeveloper.app@gmail.com>
2026-04-05 20:23:20 -0300

    clean: remove pasta public vazia do template

--- electron/main.ts
+++ electron/main.ts
@@ -7,12 +7,6 @@ process.env.APP_ROOT = path.join(__dirname, '..')
 const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL']
 const RENDERER_DIST = path.join(process.env.APP_ROOT, 'dist')
 
-const VITE_PUBLIC = VITE_DEV_SERVER_URL
-  ? path.join(process.env.APP_ROOT, 'public')
-  : RENDERER_DIST
-
-process.env.VITE_PUBLIC = VITE_PUBLIC
-
 let win: BrowserWindow | null = null
 
 
